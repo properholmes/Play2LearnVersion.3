a91b6f4bfd28d94030cfc9d2a4c5f6bfc3cd42db
wats0nUnMEduB13 <nghtrsnl@gmail.com>
2024-10-07 15:04:53 -0400

    Add margin to hamburger menu and hamburger menu items

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,14 +4,15 @@ function Header() {
   return (
     <>
       <header>
-        <nav className="nav-container navbar navbar-expand-lg navbar-light bg-light fixed-top">
-          <a className="navbar-brand" href="./">Play2Learn</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+          <a className="navbar-brand ms-3" href="./">Play2Learn</a>
+          <button className="navbar-toggler me-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">   
+
+            <ul className="navbar-nav mr-auto ms-3">
               <li className="nav-item active">
                 <a className="nav-link" href="./">Home<span className="sr-only">(current)</span></a>
               </li>
