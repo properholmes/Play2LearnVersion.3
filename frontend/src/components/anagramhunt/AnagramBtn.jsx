3f569b34cd635b5507c9cd614991b3880b9a6268
wats0nUnMEduB13 <nghtrsnl@gmail.com>
2024-10-10 13:43:41 -0400

    change frontend to final for better folder naming

--- frontend/src/components/anagramhunt/AnagramBtn.jsx
+++
@@ -1,9 +0,0 @@
-import {Link} from 'react-router-dom'
-
-function AnagramBtn(props) {
-    return (
-       <Link className={props.bootstrapClass} id="playButton" to={props.route}> {props.label}</Link>
-    )
-}
-
-export default AnagramBtn;
--- frontend/src/components/anagramhunt/Left.jsx
+++
@@ -1,9 +0,0 @@
-function Left(props) {
-    return (
-        <>
-        <h3>{props.wordsLeft} words left</h3>
-        </>
-    )
-}
-
-export default Left;
